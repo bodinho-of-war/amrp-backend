c550352197d249d8f699e312b79c2678fa9ac95c
luan <luanpb@live.com>
2021-12-30 17:51:41 -0300

    remove build folder

--- build/src/database/Db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Db;
-(function (Db) {
-    Db[Db["MONGODB"] = 0] = "MONGODB";
-})(Db || (Db = {}));
-exports.default = Db;
